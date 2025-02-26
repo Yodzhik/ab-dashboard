6a8cd2c84163e77c20027d7697e91347423139c1
Alim Chechenov <chechenov321@gmail.com>
2025-02-27 00:22:58 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Dashboard } from './components/Dashboard/Dashboard';
 import { Results } from './components/Results/Results';
--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from 'react';
 import {useNavigate} from 'react-router-dom';
-import './Dashboard.css';
 
 export const Dashboard = () => {
     const [tests, setTests] = useState([]);
